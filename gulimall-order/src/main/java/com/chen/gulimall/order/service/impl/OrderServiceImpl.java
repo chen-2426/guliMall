d90a1faeac6ac74d3fa2fab1a68205f42de5faef
chen-2426 <574311203@qq.com>
2023-10-20 11:16:43 +0800

    完善支付功能

--- gulimall-order/src/main/java/com/chen/gulimall/order/service/impl/OrderServiceImpl.java
+++ gulimall-order/src/main/java/com/chen/gulimall/order/service/impl/OrderServiceImpl.java
@@ -1,20 +1,31 @@
 package com.chen.gulimall.order.service.impl;
 
+import com.chen.gulimall.base.Constant.CartConstant;
 import com.chen.gulimall.base.VO.MemberRespVo;
+import com.chen.gulimall.order.constant.OrderConstant;
+import com.chen.gulimall.order.entity.OrderItemEntity;
 import com.chen.gulimall.order.feign.CartFeign;
 import com.chen.gulimall.order.feign.MemberFeign;
 import com.chen.gulimall.order.interceptor.LoginUserInterceptor;
-import com.chen.gulimall.order.vo.MemberAddressVo;
-import com.chen.gulimall.order.vo.OrderConfirmVo;
-import com.chen.gulimall.order.vo.OrderItemVo;
+import com.chen.gulimall.order.vo.*;
+import io.seata.spring.annotation.GlobalTransactional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -25,14 +36,20 @@ import com.chen.gulimall.base.utils.Query;
 import com.chen.gulimall.order.dao.OrderDao;
 import com.chen.gulimall.order.entity.OrderEntity;
 import com.chen.gulimall.order.service.OrderService;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
 
 @Service("orderService")
 public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> implements OrderService {
+    private ThreadLocal<OrderSubmitVO> confirmVoTreadLocak =  new ThreadLocal<>();
     @Autowired
     ThreadPoolExecutor executor;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    //RedisTemplate 存储字节码，不可读
 
     @Autowired
     MemberFeign memberFeign;
@@ -47,28 +64,167 @@ public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> impleme
 
         return new PageUtils(page);
     }
-
     @Override
-    public OrderConfirmVo confirmOrder() throws ExecutionException, InterruptedException {
-        MemberRespVo memberRespVo = LoginUserInterceptor.loginUser.get();
+    public OrderConfirmVo confirmOrder() {
+        MemberRespVo memberResponseVo = LoginUserInterceptor.loginUser.get();
         OrderConfirmVo confirmVo = new OrderConfirmVo();
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        CompletableFuture<Void> memberFuture = CompletableFuture.runAsync(() -> {
+        CompletableFuture<Void> itemAndStockFuture = CompletableFuture.supplyAsync(() -> {
             RequestContextHolder.setRequestAttributes(requestAttributes);
-            List<MemberAddressVo> address = memberFeign.getAddress(memberRespVo.getId());
-            confirmVo.setAddresses(address);
+            //1. 查出所有选中购物项
+            List<OrderItemVo> checkedItems = cartFeignService.getCheckedItems();
+            confirmVo.setItems(checkedItems);
+            return checkedItems;
+        }, executor).thenAcceptAsync((items) -> {
+            //4. 库存
+            List<Long> skuIds = items.stream().map(OrderItemVo::getSkuId).collect(Collectors.toList());
+            Map<Long, Boolean> hasStockMap = wareFeignService.getSkuHasStocks(skuIds).stream().collect(Collectors.toMap(SkuHasStockVo::getSkuId, SkuHasStockVo::getHasStock));
+            confirmVo.setStocks(hasStockMap);
         }, executor);
-        CompletableFuture<Void> cartFuture = CompletableFuture.runAsync(() -> {
-            RequestContextHolder.setRequestAttributes(requestAttributes);
-            List<OrderItemVo> currentUserCartItems = cartFeign.getCurrentUserCartItems();
-            confirmVo.setItems(currentUserCartItems);
+
+        //2. 查出所有收货地址
+        CompletableFuture<Void> addressFuture = CompletableFuture.runAsync(() -> {
+            List<MemberAddressVo> addressByUserId = memberFeignService.getAddressByUserId(memberResponseVo.getId());
+            confirmVo.setMemberAddressVos(addressByUserId);
         }, executor);
-        Integer integration = LoginUserInterceptor.loginUser.get().getIntegration();
-        confirmVo.setIntegration(integration);
-        //防重复令牌 todo
-        CompletableFuture.allOf(memberFuture, cartFuture).get();
+
+        //3. 积分
+        confirmVo.setIntegration(memberResponseVo.getIntegration());
+
+        //5. 总价自动计算
+        //6. 防重令牌
+        String token = UUID.randomUUID().toString().replace("-", "");
+        redisTemplate.opsForValue().set(OrderConstant.USER_ORDER_TOKEN_PREFIX + memberResponseVo.getId(), token, 30, TimeUnit.MINUTES);
+        confirmVo.setOrderToken(token);
+        try {
+            CompletableFuture.allOf(itemAndStockFuture, addressFuture).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
         return confirmVo;
     }
 
+    @Override
+    public void handlerPayResult(PayAsyncVo payAsyncVo) {
+
+    }
+
+    //    @GlobalTransactional
+    @Transactional
+    @Override
+    public SubmitOrderResponseVo submitOrder(OrderSubmitVo submitVo) {
+        SubmitOrderResponseVo responseVo = new SubmitOrderResponseVo();
+        responseVo.setCode(0);
+        //1. 验证防重令牌
+        MemberResponseVo memberResponseVo = LoginInterceptor.loginUser.get();
+        String script= "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        Long execute = redisTemplate.execute(new DefaultRedisScript<>(script,Long.class), Arrays.asList(OrderConstant.USER_ORDER_TOKEN_PREFIX + memberResponseVo.getId()), submitVo.getOrderToken());
+        if (execute == 0L) {
+            //1.1 防重令牌验证失败
+            responseVo.setCode(1);
+            return responseVo;
+        }else {
+            //2. 创建订单、订单项
+            OrderCreateTo order =createOrderTo(memberResponseVo,submitVo);
+
+            //3. 验价
+            BigDecimal payAmount = order.getOrder().getPayAmount();
+            BigDecimal payPrice = submitVo.getPayPrice();
+            if (Math.abs(payAmount.subtract(payPrice).doubleValue()) < 0.01) {
+                //4. 保存订单
+                saveOrder(order);
+                //5. 锁定库存
+                List<OrderItemVo> orderItemVos = order.getOrderItems().stream().map((item) -> {
+                    OrderItemVo orderItemVo = new OrderItemVo();
+                    orderItemVo.setSkuId(item.getSkuId());
+                    orderItemVo.setCount(item.getSkuQuantity());
+                    return orderItemVo;
+                }).collect(Collectors.toList());
+                WareSkuLockVo lockVo = new WareSkuLockVo();
+                lockVo.setOrderSn(order.getOrder().getOrderSn());
+                lockVo.setLocks(orderItemVos);
+                R r = wareFeignService.orderLockStock(lockVo);
+                //5.1 锁定库存成功
+                if (r.getCode()==0){
+//                    int i = 10 / 0;
+                    responseVo.setOrder(order.getOrder());
+                    responseVo.setCode(0);
+
+                    //发送消息到订单延迟队列，判断过期订单
+                    rabbitTemplate.convertAndSend("order-event-exchange","order.create.order",order.getOrder());
+
+                    //清除购物车记录
+                    BoundHashOperations<String, Object, Object> ops = redisTemplate.boundHashOps(CartConstant.CART_PREFIX + memberResponseVo.getId());
+                    for (OrderItemEntity orderItem : order.getOrderItems()) {
+                        ops.delete(orderItem.getSkuId().toString());
+                    }
+                    return responseVo;
+                }else {
+                    //5.1 锁定库存失败
+                    String msg = (String) r.get("msg");
+                    throw new NoStockException(msg);
+                }
+
+            }else {
+                //验价失败
+                responseVo.setCode(2);
+                return responseVo;
+            }
+        }
+    }
+
+    @Override
+    public OrderEntity getOrderByOrderSn(String orderSn) {
+        OrderEntity order_sn = this.getOne(new QueryWrapper<OrderEntity>().eq("order_sn", orderSn));
+
+        return order_sn;
+    }
+
+    /**
+     * 关闭过期的的订单
+     * @param orderEntity
+     */
+    @Override
+    public void closeOrder(OrderEntity orderEntity) {
+        //因为消息发送过来的订单已经是很久前的了，中间可能被改动，因此要查询最新的订单
+        OrderEntity newOrderEntity = this.getById(orderEntity.getId());
+        //如果订单还处于新创建的状态，说明超时未支付，进行关单
+        if (newOrderEntity.getStatus() == OrderStatusEnum.CREATE_NEW.getCode()) {
+            OrderEntity updateOrder = new OrderEntity();
+            updateOrder.setId(newOrderEntity.getId());
+            updateOrder.setStatus(OrderStatusEnum.CANCLED.getCode());
+            this.updateById(updateOrder);
+
+            //关单后发送消息通知其他服务进行关单相关的操作，如解锁库存
+            OrderTo orderTo = new OrderTo();
+            BeanUtils.copyProperties(newOrderEntity,orderTo);
+            rabbitTemplate.convertAndSend("order-event-exchange", "order.release.other",orderTo);
+        }
+    }
+//    @GlobalTransactional
+//    @Transactional
+//    @Override
+//    public OrderConfirmVo confirmOrder() throws ExecutionException, InterruptedException {
+//        MemberRespVo memberRespVo = LoginUserInterceptor.loginUser.get();
+//        OrderConfirmVo confirmVo = new OrderConfirmVo();
+//        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+//        CompletableFuture<Void> memberFuture = CompletableFuture.runAsync(() -> {
+//            RequestContextHolder.setRequestAttributes(requestAttributes);
+//            List<MemberAddressVo> address = memberFeign.getAddress(memberRespVo.getId());
+//            confirmVo.setAddresses(address);
+//        }, executor);
+//        CompletableFuture<Void> cartFuture = CompletableFuture.runAsync(() -> {
+//            RequestContextHolder.setRequestAttributes(requestAttributes);
+//            List<OrderItemVo> currentUserCartItems = cartFeign.getCurrentUserCartItems();
+//            confirmVo.setItems(currentUserCartItems);
+//        }, executor);
+//        Integer integration = LoginUserInterceptor.loginUser.get().getIntegration();
+//        confirmVo.setIntegration(integration);
+//        //防重复令牌 todo
+//        CompletableFuture.allOf(memberFuture, cartFuture).get();
+//        return confirmVo;
+//    }
 
 }
